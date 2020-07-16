2572d6469de764516f22f5556ae511b146813978
Jacquelynne <jacheima@uat.edu>
2020-07-16 00:05:06 -0700

    Created the Project
    Created the project and got the background and player controller into the scene. Then I got the player controller set up so we can move the player around the scene.

---
+++ UltimateAsteroidsGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float rotateSpeed;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal"); //-1, go left --- 1, go right
+        float vertical = Input.GetAxis("Vertical"); // -1, go down --- 1, go up
+
+        Vector3 myPos = transform.position;
+        myPos += transform.up * vertical * speed * Time.deltaTime;
+        transform.position = myPos;
+
+        transform.Rotate(transform.forward * horizontal * rotateSpeed * Time.deltaTime * -1);
+    }
+}
